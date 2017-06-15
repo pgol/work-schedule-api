d0ee31a8f388b505c301dfa485d706870c25ab14
pgol <pgol200014@gmail.com>
2017-06-15 10:55:44 +0200

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3002;
@@ -9,6 +10,7 @@ const router = require('./routes/router');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 
 app.use('/api/v1', router);
