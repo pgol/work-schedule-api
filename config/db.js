f2fc6a652ea75342617f6fe9307e3d0771359804
Pawel Gol <gol.pawel92@gmail.com>
2017-10-28 20:01:32 +0200

    Fix Knex config

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const knex = require('knex')(require('../config/knexfile'));
+const knex = require('knex')(require('../knexfile'));
 
 module.exports = {
   knex
--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-const {db} = require('./config');
+const {db} = require('./config/config');
 
 module.exports = {
   client: 'pg',
